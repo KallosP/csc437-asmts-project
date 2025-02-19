7622cbde1a6da030fe2757d8abec799d965d5d54
KallosP <pkallos19@gmail.com>
2025-02-19 09:42:31 -0800

    login/create account page modifications; lab 16 setup

--- packages/project-frontend/src/pages/LoginPage.jsx
+++ packages/project-frontend/src/pages/LoginPage.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react'
 export default function LoginPage() {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [createNewAccount, setCreateNewAccount] = useState(false)
 
 	const handleLogin = (e) => {
+		console.log(e)
 		e.preventDefault()
 		console.log('Login clicked')
 	}
@@ -13,7 +15,7 @@ export default function LoginPage() {
 		<div className="flex self-center h-full justify-center items-center w-full p-4">
 			<div className="w-full mb-8 max-w-lg rounded-lg bg-elevated-background dark:bg-dark-elevated-background p-8 shadow-lg">
 				<h2 className="mb-6 text-center text-2xl text-normal-text dark:text-dark-normal-text font-semibold">
-					Login
+					{createNewAccount ? 'Create an account' : 'Login'}
 				</h2>
 				<form onSubmit={handleLogin} className="space-y-4">
 					{/* Email Input */}
@@ -43,20 +45,39 @@ export default function LoginPage() {
 							className="w-full px-3 dark:text-dark-normal-text text-normal-text placeholder:text-hint-text py-2 border-2 border-input-border rounded-lg focus:outline-none focus:ring-2 focus:ring-input-border-ring dark:focus:ring-dark-input-border-ring dark:border-dark-input-border "
 						/>
 					</div>
-					<button
-						className="w-full text-button-text dark:text-dark-button-text cursor-pointer transition-all duration-300 bg-button-background dark:bg-dark-button-background hover:bg-button-hover dark:hover:bg-dark-button-hover focus:bg-button-focus dark:focus:bg-dark-button-focus font-medium rounded-lg text-sm px-4 py-2 "
-						type="submit">
-						Login
-					</button>
+					{// TODO: different action depending on login or create button
+					!createNewAccount ? (
+						<button
+							className="w-full text-button-text dark:text-dark-button-text cursor-pointer transition-all duration-300 bg-button-background dark:bg-dark-button-background hover:bg-button-hover dark:hover:bg-dark-button-hover focus:bg-button-focus dark:focus:bg-dark-button-focus font-medium rounded-lg text-sm px-4 py-2 "
+							type="submit">
+							Login
+						</button>
+					) : (
+						<button
+							className="w-full text-button-text dark:text-dark-button-text cursor-pointer transition-all duration-300 bg-button-background dark:bg-dark-button-background hover:bg-button-hover dark:hover:bg-dark-button-hover focus:bg-button-focus dark:focus:bg-dark-button-focus font-medium rounded-lg text-sm px-4 py-2 "
+							type="submit">
+							Create Account
+						</button>
+					)}
 				</form>
-				<p className="mt-4 text-center text-sm text-normal-text dark:text-dark-normal-text">
-					Don't have an account?
-					<a
-						href="/signup"
-						className="ml-1 font-medium text-link-text dark:text-dark-link-text hover:underline">
-						Create an account
-					</a>
-				</p>
+				{!createNewAccount ? (
+					<p className="mt-4 text-center text-sm text-normal-text dark:text-dark-normal-text">
+						Don't have an account?
+						<a
+							onClick={() => setCreateNewAccount(true)}
+							className="ml-1 cursor-pointer font-medium text-link-text dark:text-dark-link-text hover:underline">
+							Create an account
+						</a>
+					</p>
+				) : (
+					<div className="text-center mt-4">
+						<a
+							onClick={() => setCreateNewAccount(false)}
+							className="cursor-pointer font-medium text-link-text dark:text-dark-link-text hover:underline">
+							Back to Login
+						</a>
+					</div>
+				)}
 			</div>
 		</div>
 	)
