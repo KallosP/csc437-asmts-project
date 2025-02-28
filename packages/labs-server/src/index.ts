127b5b7bd99c7584ccfdc631d6c593f48c9327d0
KallosP <pkallos19@gmail.com>
2025-02-28 10:53:36 -0800

    lab 20 progress save

--- packages/labs-server/src/index.ts
+++ packages/labs-server/src/index.ts
@@ -1,11 +1,26 @@
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
+import { MongoClient } from 'mongodb'
 
 dotenv.config() // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000
 const staticDir = path.resolve(process.env.STATIC_DIR || 'public') // Ensure an absolute path
+async function setUpServer() {
 
+	const { MONGO_USER, MONGO_PWD, MONGO_CLUSTER, DB_NAME } = process.env
+
+	const connectionStringRedacted = `mongodb+srv://${MONGO_USER}:<password>@${MONGO_CLUSTER}/${DB_NAME}`
+	const connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_CLUSTER}/${DB_NAME}`
+
+	console.log('Attempting Mongo connection at ' + connectionStringRedacted)
+
+	const mongoClient = await MongoClient.connect(connectionString)
+	const collectionInfos = await mongoClient.db().listCollections().toArray()
+	console.log(collectionInfos.map((collectionInfo) => collectionInfo.name)) // For debug only
+}
+
+setUpServer()
 
 const app = express()
 
