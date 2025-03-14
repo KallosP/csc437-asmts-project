024c45237b79bfda141e9073d2177f1413e44d91
KallosP <pkallos19@gmail.com>
2025-03-14 10:35:07 -0700

    changed to prod url

--- packages/project-frontend/src/constants.ts
+++ packages/project-frontend/src/constants.ts
@@ -1,4 +1,4 @@
-//const prodURL = ""
+const PROD_URL = "https://pkallos.csse.dev"
 const LOCAL_URL = "http://localhost:3000";
-const BACKEND_URL = LOCAL_URL
+const BACKEND_URL = PROD_URL;
 export default BACKEND_URL;
